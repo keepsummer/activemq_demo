b416d84a26e8f98da06eb28bb0728caf3343b747
李志敏 <915377975@qq.com>
2020-02-04 17:45:03 +0800

    创建连接

--- activemq_demo/src/main/java/com/lizhimin/activemq/ActiveMQDemo.java
+++ activemq_demo/src/main/java/com/lizhimin/activemq/ActiveMQDemo.java
@@ -1,9 +1,11 @@
 package com.lizhimin.activemq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 public class ActiveMQDemo {
     public static void main(String[] args){
         //1、创建连接工厂
 
-
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
     }
 }
